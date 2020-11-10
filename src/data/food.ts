c49a07dbe1ca5a60487718c4de0db420c5f5c6ae
adb <alexdebeaudrap@gmail.com>
2020-11-10 14:47:17 +0100

    test de testage 2

--- src/data/food.ts
+++ src/data/food.ts
@@ -13,11 +13,7 @@ export interface FoodItem {
 
 // values from: http://dieteplaisir.canalblog.com/archives/2012/05/18/24268064.html
 export const FOOD_ITEMS: Array<FoodItem> = [
-  {
-    name: "Sushi",
-    kcal: 45,
-    imageUrl: Sushi
-  },  
+
   {
     name: "Sushi Alex",
     kcal: 45,
